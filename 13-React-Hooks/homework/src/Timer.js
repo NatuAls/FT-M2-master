084fe2fbf6ecac4ac22488e34c7b37b552fb18fb
NatuAls <nahuelalesso19@gmail.com>
2022-10-18 18:39:39 -0300

    homework13 complete

--- 13-React-Hooks/homework/src/Timer.js
+++ 13-React-Hooks/homework/src/Timer.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import './Timer.css'
 
 const Timer = () => {
+  const [segundos, setSegundos] = useState(0);
+  const [activo, setActivo] = useState(false);
+  const [tipo, setTipo] = useState('Contador');
+
+  const myRef = useRef(null);
+  function agregaSegundos(){
+    let ref = myRef.current.value;
+    if(ref > 0)setSegundos(ref);
+  }
+
+  function cambioTipo(){
+    tipo === 'Contador' ? setTipo('Cuenta Regresiva') : setTipo('Contador');
+  }
+
+  function toggle(){
+    setActivo(!activo);
+  }
+
+  function reset(){
+    setSegundos(0);
+    setActivo(false);
+    // extra
+    myRef.current.value = '';
+  }
+
+  useEffect(() => {
+    let intervalo = null;
+    if (activo && tipo === 'Contador') {
+      intervalo = setInterval(() => {
+        setSegundos(segundos => segundos + 1);
+      }, 1000);
+    }
+    if (activo && tipo === 'Cuenta Regresiva') {
+      intervalo = setInterval(() => {
+        setSegundos(segundos => segundos - 1);
+      }, 1000);
+    }
+    if (!activo && segundos !== 0 && tipo === 'Contador') {
+      clearInterval(intervalo);
+    }
+    if (segundos === 0 && tipo === 'Cuenta Regresiva') {
+      reset();
+      clearInterval(intervalo);
+    }
+
+    return () => clearInterval(intervalo);
+  });
+
+
   return (
     <div className="app">
-      Componente Timer
+      <div className="time">
+        {segundos}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${activo ? 'active' : 'inactive'}`} onClick={toggle}>
+          {activo ? 'Pausa' : 'Inicio'}
+        </button>
+        <button className="button-secondary" onClick={reset}>
+          Reset
+        </button>
+      </div>
+      <button className="button" onClick={cambioTipo}>
+          {tipo}
+      </button>
+      {tipo === 'Cuenta Regresiva' && <input type="number" ref={myRef} onChange={agregaSegundos} placeholder="Ingresa Segundos" autoComplete="off"/>}
     </div>
   );
 };
